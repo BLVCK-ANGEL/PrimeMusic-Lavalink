e965b77b6400a2cd113822925dccfff5edc22078
BlvckAngel <83200567+BLVCK-ANGEL@users.noreply.github.com>
2025-03-11 13:47:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  ownerID: ["818920751444393994", ""], 
+  mongodbUri : "mongodb+srv://PHV-CLUSTER:PHV04@phv-cloud.1bg1tl8.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret : "199a619d22dd4e55a4a2c1a7a3d70e63",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "#1db954",
+  embedColor: "#ff2e05",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
