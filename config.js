9868f311b74ad307b2d2faba6eb429ecd2fe0ace
BlvckAngel <83200567+BLVCK-ANGEL@users.noreply.github.com>
2025-03-11 14:06:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "XX",
-      password: "enteryourcustompass",
-      host: "180.188.226.76",
-      port:  7019,
+      name: "Main",
+      password: "horizxon.tech",
+      host: "lava.horizxon.tech",
+      port:  80,
       secure: false
     }
   ]
